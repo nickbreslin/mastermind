febf4ce66e3a12a416d858e1e5cfe2f7f8273b90
Nick Breslin <nickbreslin@gmail.com>
2019-02-16 01:38:59 -0800

    Director and UI Manager

--- Assets/Scripts/MonoSingleton.cs
+++ Assets/Scripts/MonoSingleton.cs
@@ -33,7 +33,7 @@ public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T
             m_Instance = this as T;
             m_Instance.Init ();
         } else {
-            //DestroyImmediate (this);
+            DestroyImmediate (this);
         }
     }
 
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIState
+{
+	None,
+	MainMenu
+
+}
+
+public class UIManager : MonoSingleton<UIManager>
+{
+	private UIState uiState;
+
+	protected override void Init ()
+    {
+        SetUIState( UIState.None );
+    }
+
+    public void isLoaded() {
+    	Debug.Log("UI Manager is attached.");
+    }
+
+	public void SetUIState( UIState state ) {
+		uiState = state;
+	}
+
+	void OnGUI() {
+
+		switch( uiState ) {
+
+			case UIState.MainMenu:
+				RenderMainMenu();
+				break;
+
+			default:
+				break;
+		}
+	}
+
+
+
+	void RenderMainMenu () {
+		
+	}
+}
