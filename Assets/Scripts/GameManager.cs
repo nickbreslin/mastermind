32925a25d61069aa3c47988a25b7c2e60cf87104
Nick Breslin <nickbreslin@gmail.com>
2019-02-16 03:51:19 -0800

    GameManager, and loading sprite through script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    void Start() {
+        GameObject go = new GameObject();
+        SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
+        renderer.sprite = Resources.Load("Minions/Minion_Fox", typeof(Sprite)) as Sprite;
+
+//        go.AddComponent<>();
+    }
+}
